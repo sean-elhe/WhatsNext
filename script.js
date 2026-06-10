213f39bb111886e62ef2bc158c9bddd3ea1fee47
sean-elhe <se09gv@gmail.com>
2026-06-10 15:53:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,180 @@
+let globalData = null;
+let bookName = "Genesis";
+let bookNameDisplay = "Genesis"
+let currentIndex = 0;
+let randomIndex = Math.floor(Math.random() * 65);
+let rightCount = 0;
+let wrongCount = 0;
+let stage = 1;
+let number = 0;
+
+const answer = document.getElementById("answer");
+
+answer.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        displayQuestion();
+    }
+});
+
+const question = document.getElementById("question")
+const firstWord = document.getElementById("firstWord")
+const secondWord = document.getElementById("secondWord")
+const thirdWord = document.getElementById("thirdWord")
+
+const nextBtn = document.getElementById("nextBtn")
+const score = document.getElementById("score")
+
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        globalData = data
+
+        number = Math.floor(Math.random() * 100);
+        displayQuestion();
+    });
+
+async function displayQuestion() {
+    let books = globalData;
+    bookName = books[randomIndex].book_name;
+
+    if (number > 50) {
+        console.log("Number is over 50: What's after?")
+        bookNameDisplay = books[randomIndex - 1].book_name;
+        thirdWord.textContent = `${bookNameDisplay}?`;
+
+
+        secondWord.classList.remove("wrong")
+        secondWord.classList.add("correct")
+        secondWord.textContent = "after"
+        checkAnswer();
+    } else {
+        console.log("Number is 50 or less: What's before?")
+        bookNameDisplay = books[randomIndex + 1].book_name;
+        thirdWord.textContent = `${bookNameDisplay}?`;
+    
+        secondWord.classList.remove("correct")
+        secondWord.classList.add("wrong")
+        secondWord.textContent = "before"
+        checkAnswer();
+    }
+
+    console.log("Current index: ", currentIndex)
+
+}
+
+async function checkAnswer(){
+    let userText = answer.value;
+    
+    if (stage === 1 && currentIndex != 0) {
+        console.log(stage)
+            if (userText.toLowerCase() === bookName.toLowerCase()) {
+                rightCount++
+                answer.classList.add("correct")
+            } else {
+                wrongCount++
+                answer.classList.add("wrong")
+            }
+        answer.value = bookName
+        stage = 2
+        nextBtn.textContent = `[${rightCount} / ${currentIndex}]`;
+        currentIndex++;
+        randomIndex = Math.floor(Math.random() * 65);
+        number = Math.floor(Math.random() * 100);
+    } else {
+        console.log("Stage:", stage)
+        answer.classList.remove("correct", "wrong")
+        answer.value = "";
+
+        stage = 1
+        nextBtn.textContent = "Check"
+    }
+
+    if (currentIndex === 0 ) {
+        currentIndex++;
+    }
+
+}
+
+async function nextBook() {
+    displayQuestion();
+};
+
+document.getElementById("nextBtn").addEventListener('click', () => {
+    nextBook();
+});
+
+// async function checkAnswerPrevious(){
+//     let userText = answer.value;
+
+//     if (stage === 1 && currentIndex != 0) {
+//         console.log(stage)
+//             if (userText.toLowerCase() === bookName.toLowerCase()) {
+//                 rightCount++
+//                 answer.classList.add("correct")
+//             } else {
+//                 wrongCount++
+//                 answer.classList.add("wrong")
+//             }
+//         answer.value = bookName
+//         stage = 2
+//         nextBtn.textContent = `[${rightCount} / ${currentIndex}]`;
+//         currentIndex++;
+//         randomIndex = Math.floor(Math.random() * 65);
+//             number = Math.floor(Math.random() * 100);
+
+
+//     } else {
+//         console.log(stage)
+//         answer.classList.remove("correct", "wrong")
+//         answer.value = "";
+
+//         stage = 1
+//         nextBtn.textContent = "Check"
+//     }
+
+//     if (currentIndex === 0 ) {
+//         currentIndex++;
+//     }
+// }
+
+
+// // async function checkAnswer(){
+// //     let books = globalData;
+// //     let bookName = books[currentIndex].book_name;
+// //     let bookNameAlt = books[currentIndex].book_name;
+
+// //     let userText = answer.value;
+
+
+// //     if (stage === 1) {
+// //         console.log(stage)
+// //             if (userText === bookName) {
+// //                 rightCount++
+// //                 answer.classList.add("correct")
+// //             } else {
+// //                 wrongCount++
+// //                 answer.classList.add("wrong")
+// //             }
+// //         answer.value = bookName;
+
+// //         stage = 2
+// //         nextBtn.textContent = "Next";
+// //         currentIndex++
+
+// //     } else {
+// //         console.log(stage)
+// //         answer.value = "";
+// //         question.textContent =
+// //         "What's after " + bookNameAlt + "?"
+
+// //         stage = 1
+// //         nextBtn.textContent = "Check"
+
+// //     }
+
+
+    
+// //     console.log(`${rightCount}/${wrongCount} `)
+// //     // console.log(userText)
+// //     // console.log(bookName)
+// // }
